9ad01fb50bcbdbb5e6ab1039fdd36945c9c3d10f
Bluish1251 <cm6of6@gmail.com>
2024-11-14 17:00:28 -0500

    11/14 workshop drive chain code

---
+++ include/umbc.h
@@ -0,0 +1,17 @@
+#ifndef _UMBC_H_
+#define _UMBC_H_
+
+#define MSG_DELAY_MS 1000
+
+#ifdef __cplusplus
+#include "umbc/controller.hpp"
+#include "umbc/controllerinput.hpp"
+#include "umbc/controllerrecorder.hpp"
+#include "umbc/pcontroller.hpp"
+#include "umbc/robot.hpp"
+#include "umbc/vcontroller.hpp"
+#include "umbc/log.hpp"
+#include "umbc/menu.hpp"
+#endif
+
+#endif // _UMBC_H_
